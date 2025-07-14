5e7afb6389bda22045bc67e70bd6b9d672b2f57a
Nuke <nukelearyt@gmail.com>
2025-07-14 00:46:28 -0400

    Day01

---
+++ README.md
@@ -0,0 +1,3 @@
+## Doing 2019 AOC in the Summer as a Friends Challenge
+
+What it says on the tin
---
+++ bin/Day01.ml
@@ -0,0 +1,19 @@
+open Core
+open Advent
+
+let fuelCalc (mass : int) : int = (mass / 3) - 2
+
+let recFuel (mass : int) : int =
+  let rec aux (mass : int) (fuel : int) : int =
+    let nFuel = fuelCalc mass in
+    if nFuel < 1 then fuel else aux nFuel (fuel + nFuel)
+  in
+  aux mass 0
+;;
+
+let () =
+  let masses = read_lines "./inputs/d01/input.txt" |> List.map ~f:int_of_string in
+  let res = List.fold masses ~init:0 ~f:(fun acc mass -> acc + fuelCalc mass) in
+  let res2 = List.fold masses ~init:0 ~f:(fun acc mass -> acc + recFuel mass) in
+  Printf.printf "\nPart 1: %i\nPart 2: %i\n" res res2
+;;
